accf5295f4cfc94a378d099952c61d93eeddc3fa
karenwang37@gmail.com <kywang@google.com>
2021-02-01 22:28:14 -0800

    now supports faster, more international doge

--- content.js
+++ content.js
@@ -1,6 +1,36 @@
+function preloadImage(url) {
+    let img=new Image();
+    img.src=url;
+}
+
+const amazonURLs = [
+	'*://*.amazon.ae/*',
+	'*://*.amazon.at/*',
+	'*://*.amazon.ca/*',
+	'*://*.amazon.cn/*',
+	'*://*.amazon.co.jp/*',
+	'*://*.amazon.co.uk/*',
+	'*://*.amazon.com.au/*',
+	'*://*.amazon.com.br/*',
+	'*://*.amazon.com.mx/*',
+	'*://*.amazon.com.sg/*',
+	'*://*.amazon.com.tr/*',
+	'*://*.amazon.com/*',
+	'*://*.amazon.de/*',
+	'*://*.amazon.es/*',
+	'*://*.amazon.fr/*',
+	'*://*.amazon.ie/*',
+	'*://*.amazon.in/*',
+	'*://*.amazon.it/*',
+	'*://*.amazon.nl/*',
+];
+
+const dogeURL = chrome.extension.getURL('images/doge.png');
+preloadImage(dogeURL);
+
 let doge = document.createElement('img'); 
 doge.id = 'theDoge'
-doge.src = chrome.extension.getURL('images/doge.png');
+doge.src = dogeURL;
 document.body.appendChild(doge);
 
 let muchCapitalism = document.createElement('span'); 
